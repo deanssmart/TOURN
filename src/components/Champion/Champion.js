ebdb646cae122bd84b4b907a5af2daef77d25010
deanssmart <deanssmart@gmail.com>
2020-09-26 18:27:57 +0100

    lasers added to champion page

--- src/components/Champion/Champion.js
+++ src/components/Champion/Champion.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Champion = ({ champion }) => (
     <>
         <h2>{ `${champion} wins!`}</h2>
+        <div class="laser-beam"></div>
+        <div class="laser-beam yellow"></div>
+        <div class="laser-beam blue-b"></div>
+        <div class="laser-beam yellow-b"></div>
     </>
 );
 
